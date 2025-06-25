0c6c33838301bda7614adbfc4f719a3fe66ff61b
Archit Rathor <117975925+Archittt@users.noreply.github.com>
2025-06-25 20:45:07 +0530

    Create Reward.js

---
+++ models/Reward.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const rewardSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  points: { type: Number, default: 0 },
+  badges: [String],
+  lastUpdated: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Reward", rewardSchema);
