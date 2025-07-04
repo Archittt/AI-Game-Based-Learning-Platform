064ea08de48302e4deff00b21a8ffbf221b211ac
Shailendra Yadav <112553682+Shailendrayadav18@users.noreply.github.com>
2025-07-04 17:02:47 +0530

    Implemented peer interaction notification feature

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const authenticate = require("./middlewares/authenticate");
 const chatbotRoutes = require("./routes/chatbotRoutes");
 const leaderboardRoutes = require("./routes/leaderboardRoutes");
 const aiIntegrationRoutes = require("./routes/aiIntegrationRoutes");
+const quizChallengeRoutes = require("./routes/quizChallengeRoutes");
 
 // Test protected route
 app.get("/api/protected", authenticate, (req, res) => {
@@ -33,7 +34,7 @@ app.use("/api", rewardRoutes);
 app.use("/api", chatbotRoutes);
 app.use("/api", leaderboardRoutes);
 app.use("/api", aiIntegrationRoutes);
-
+app.use("/api/quiz", quizChallengeRoutes);
 
 
 // Create HTTP server and initialize Socket.IO
@@ -49,6 +50,16 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log(" New client connected:", socket.id);
 
+  socket.on("join", (userId) => {
+    socket.join(userId);
+    console.log(" User ${userId} joined their room");
+  });
+
+  socket.on("join_room", (room) => {
+    socket.join(room);
+    console.log(" User join their room ${room}");
+  });
+
   socket.on("score-update", (data) => {
     console.log(" Score update received:", data);
     // Broadcast to all clients
@@ -63,6 +74,9 @@ io.on("connection", (socket) => {
     console.log(" Client disconnected:", socket.id);
   });
 });
+
+//Export io to use in routes 
+app.set('io', io);
 
 // Connect to MongoDB and start server
 mongoose.connect(process.env.MONGO_URI)
